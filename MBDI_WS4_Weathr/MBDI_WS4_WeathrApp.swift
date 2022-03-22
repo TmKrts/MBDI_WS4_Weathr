6a22a3b1eb24543eeb6b069ad3308a705bea8fe3
Tim Kaerts <tkaerts@avans.nl>
2022-03-22 14:12:44 +0100

    Initial Commit

---
+++ MBDI_WS4_Weathr/MBDI_WS4_WeathrApp.swift
@@ -0,0 +1,17 @@
+//
+//  MBDI_WS4_WeathrApp.swift
+//  MBDI_WS4_Weathr
+//
+//  Created by Tim Kaerts on 22/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct MBDI_WS4_WeathrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
